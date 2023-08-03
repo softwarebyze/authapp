757abe031744ddb90dc498fc659b9c83a9bb4155
softwarebyze <zackebenfeld@gmail.com>
2023-08-03 14:31:36 -0400

    extract email check to own function and handle err

--- server/app.py
+++ server/app.py
@@ -13,6 +13,20 @@ def get_database_connection():
     return psycopg2.connect(dbname=DB_NAME, user=DB_USER)
 
 
+def is_email_available(email):
+    # Create a new connection and cursor for each request
+    conn = get_database_connection()
+    cur = conn.cursor()
+
+    cur.execute("SELECT email FROM users WHERE email = %s", (email,))
+    users = cur.fetchall()
+
+    cur.close()
+    conn.close()
+
+    return not bool(users)
+
+
 @app.get("/users")
 def get_users():
     # Create a new connection and cursor for each request
@@ -37,27 +51,39 @@ def register():
     email = data.get("email")
     password = data.get("password")
 
+    # Check if the email is available for registration
+    if not is_email_available(email):
+        response = {"status": "error", "message": "Email already exists."}
+        return jsonify(response)
+
     # Create a new connection and cursor for each request
     conn = get_database_connection()
     cur = conn.cursor()
 
-    cur.execute(
-        "CREATE TABLE IF NOT EXISTS users (id serial PRIMARY KEY, name varchar, email varchar, password varchar);"
-    )
-    # Add user to table
-    cur.execute(
-        "INSERT INTO users (name, email, password) VALUES (%s, %s, %s)",
-        (name, email, password),
-    )
-    conn.commit()
+    try:
+        cur.execute(
+            "CREATE TABLE IF NOT EXISTS users (id serial PRIMARY KEY, name varchar, email varchar, password varchar);"
+        )
+        # Add user to table
+        cur.execute(
+            "INSERT INTO users (name, email, password) VALUES (%s, %s, %s)",
+            (name, email, password),
+        )
+        conn.commit()
 
-    cur.close()
-    conn.close()
+        response = {
+            "status": "success",
+            "message": "Your account has been created.",
+        }
 
-    response = {
-        "status": "success",
-        "message": "Your account has been created.",
-    }
+    except Exception as e:
+        # If any error occurs during the database operation, rollback the transaction
+        conn.rollback()
+        response = {"status": "error", "message": "Error registering user: " + str(e)}
+
+    finally:
+        cur.close()
+        conn.close()
 
     return jsonify(response)
 
@@ -69,27 +95,13 @@ def check_email():
     data = request.get_json()
     email = data.get("email")
 
-    # Create a new connection and cursor for each request
-    conn = get_database_connection()
-    cur = conn.cursor()
-
-    cur.execute(
-        "SELECT email FROM users WHERE email = %s",
-        (email,),
-    )
-    users = cur.fetchall()
-
-    cur.close()
-    conn.close()
-
-    if not users:
-        # If the email is not in the database, send a success message
+    # Check if the email is available for registration
+    if not is_email_available(email):
+        response = {"status": "error", "message": "Email already exists."}
+    else:
         response = {
             "status": "success",
             "message": "Email is available for registration.",
         }
-    else:
-        # If the email already exists in the database, send an error message
-        response = {"status": "error", "message": "Email already exists."}
 
     return jsonify(response)
