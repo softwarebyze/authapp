b7c7a933a60b82fa2aec1cb849d3b8ffc5168444
softwarebyze <zackebenfeld@gmail.com>
2023-08-05 12:00:44 -0400

    creating issues from frontend is working

--- client/app/components/issue-form.tsx
+++ client/app/components/issue-form.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { Button, Chip, Container, TextField, Typography } from "@mui/material";
-import { useState } from "react";
+import { FormEvent, useState } from "react";
 
 interface IssueForm {
   title: string;
@@ -15,59 +15,115 @@ export default function IssueForm() {
     labels: [],
   });
   const [label, setLabel] = useState("");
+  const [error, setError] = useState("");
+  const [message, setMessage] = useState("");
+
   const addLabel = () => {
     setForm({ ...form, labels: [...form.labels, label] });
     setLabel("");
   };
+  const handleCreateIssue = async (event: FormEvent<HTMLFormElement>) => {
+    // Stop the form from submitting and refreshing the page.
+    event.preventDefault();
+
+    const data = form;
+
+    // Send the data to the server in JSON format.
+    const JSONdata = JSON.stringify(data);
+
+    // API endpoint where we send form data.
+    const endpoint = "http://127.0.0.1:5000/create-issue";
+
+    const options = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*",
+      },
+      body: JSONdata,
+    };
+
+    const response = await fetch(endpoint, options);
+    const result = await response.json();
+
+    if (result.status !== "success") {
+      setError("Something went wrong.");
+      console.error(result);
+    } else {
+      setMessage(result.message);
+    }
+  };
   return (
-    <Container sx={{ display: "flex", flexDirection: "column", mt: 4 }}>
-      <Typography variant="h6">Create an issue</Typography>
-      <TextField
-        sx={{ my: 1 }}
-        type="text"
-        value={form.title}
-        onChange={(e) => setForm({ ...form, title: e.target.value })}
-        label="Issue Title"
-        variant="outlined"
-        color="primary"
-      />
-      <TextField
-        sx={{ my: 1 }}
-        type="text"
-        value={form.body}
-        onChange={(e) => setForm({ ...form, body: e.target.value })}
-        label="Issue Body"
-        variant="outlined"
-        color="primary"
-      />
-      <Container sx={{ display: "flex", flexDirection: "column", my: 1 }}>
-        <Container sx={{ display: "flex", alignItems: "center" }}>
-          <TextField
-            type="text"
-            value={label}
-            onChange={(e) => setLabel(e.target.value)}
-            label="Issue Label"
-            variant="outlined"
-            color="primary"
-          />
-          <Button
-            sx={{ mx: 1 }}
-            onClick={addLabel}
-            variant="contained"
-            color="primary"
-          >
-            Add Label
-          </Button>
+    <form onSubmit={handleCreateIssue}>
+      <Container sx={{ display: "flex", flexDirection: "column", mt: 4 }}>
+        <Typography variant="h6">Create an issue</Typography>
+        <TextField
+          sx={{ my: 1 }}
+          type="text"
+          value={form.title}
+          onChange={(e) => setForm({ ...form, title: e.target.value })}
+          label="Issue Title"
+          variant="outlined"
+          color="primary"
+        />
+        <TextField
+          sx={{ my: 1 }}
+          type="text"
+          value={form.body}
+          onChange={(e) => setForm({ ...form, body: e.target.value })}
+          label="Issue Body"
+          variant="outlined"
+          color="primary"
+        />
+        <Container sx={{ display: "flex", flexDirection: "column", my: 1 }}>
+          <Container sx={{ display: "flex", alignItems: "center" }}>
+            <TextField
+              type="text"
+              value={label}
+              onChange={(e) => setLabel(e.target.value)}
+              label="Issue Label"
+              variant="outlined"
+              color="primary"
+            />
+            <Button
+              sx={{ mx: 1 }}
+              onClick={addLabel}
+              variant="contained"
+              color="primary"
+            >
+              Add Label
+            </Button>
+          </Container>
+          <Container sx={{ display: "flex", mt: 1 }}>
+            {form.labels.map((label, id) => (
+              <Chip sx={{ mx: 1 }} key={id} label={label} />
+            ))}
+          </Container>
         </Container>
-        <Container sx={{ display: "flex", mt: 1 }}>
-          {form.labels.map((label, id) => (
-            <Chip sx={{ mx: 1 }} key={id} label={label} />
-          ))}
-        </Container>
+        {error && (
+          <Typography color="red" variant="body1">
+            {error}
+          </Typography>
+        )}
+        {message && (
+          <Typography color="green" variant="body1">
+            {message}
+          </Typography>
+        )}
+        {message === "GitHub issue created successfully." && (
+          <Typography variant="body1">
+            <a
+              href="
+          https://github.com/softwarebyze/authapp/issues"
+            >
+              View issue on GitHub
+            </a>
+          </Typography>
+        )}
+        <Button type="submit" variant="contained" color="primary">
+          Create Issue
+        </Button>
       </Container>
-      <Button variant="contained" color="primary">
-        Create Issue
-      </Button>
-    </Container>
+    </form>
   );
 }
